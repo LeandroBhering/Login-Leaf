0639da133b727307c41cb9e9710ebef0b2d56f5d
LeandroBhering <leandrobenega@gmail.com>
2023-04-05 21:56:19 -0300

    Update script.js

--- login/script.js
+++ login/script.js
@@ -2,8 +2,8 @@
 const logintxt = document.getElementById('login')
 const senhatxt = document.getElementById('senha')
 
-var login = 'leandrobhering'
-var senha = 'senha123'
+var login = 'admin'
+var senha = 'admin'
 
 function entrar(){
     if ((logintxt.value === login) && (senhatxt.value === senha)){
